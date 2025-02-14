a4869a59ee3d6e0810ce11c5d1a6b41a7dc40dd2
Diego Villatoro <davillatoro@scl.edu.gt>
2025-02-13 21:49:12 -0600

    version 1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("beso-btn").addEventListener("click", function() {
+    document.getElementById("mensaje").classList.remove("hidden");
+    document.getElementById("paulo-img").src = "https://i.pinimg.com/736x/e6/36/95/e63695df67e7df745ef0e66e68329863.jpg"; // Cambia la imagen a una donde mande un beso
+});
